35eb34fac7340bb4902ff6f6b1901a88e5d4c6cc
poohootdev <poohoot.dev@gmail.com>
2022-08-04 06:45:08 +0900

    도전 리스트 아이템, 아바타, 닉네임 다른 유저 정보도 정상적으로 보이도록 수정

--- src/components/ChallengesListItem.jsx
+++ src/components/ChallengesListItem.jsx
@@ -1,40 +1,38 @@
-import { useSelector } from 'react-redux';
+import { useState, useCallback } from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import PropTypes from 'prop-types';
+import '../../src/firebase';
+import { get, child, ref, getDatabase } from 'firebase/database';
 
 function ChallengesListItem({ uid, timestamp, image, description }) {
-  const { user } = useSelector((state) => state);
+  const [avatar, setAvatar] = useState('');
+  const [name, setName] = useState('');
 
-  /*eslint no-undef: "error"*/
   const relativeTime = require('dayjs/plugin/relativeTime');
   dayjs.extend(relativeTime);
   dayjs.locale('ko');
 
   const displayDate = dayjs(timestamp).fromNow() + ' | ' + dayjs(timestamp).format('YYYY/MM/DD');
 
+  const getAnotherUserInfo = useCallback(async (uid) => {
+    const snapShot = await get(child(ref(getDatabase()), 'users/' + uid));
+
+    setAvatar(snapShot.val().avatar);
+    setName(snapShot.val().name);
+  }, []);
+
+  getAnotherUserInfo(uid);
+
   return (
     <Card sx={{ maxWidth: 400, mb: 4 }}>
-      <CardHeader
-        avatar={
-          user.currentUser.uid === uid ? (
-            <Avatar src={user.currentUser?.photoURL} />
-          ) : (
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
-            </Avatar>
-          )
-        }
-        title={user.currentUser.uid === uid ? user.currentUser.displayName : uid}
-        subheader={displayDate}
-      />
+      <CardHeader avatar={<Avatar src={avatar} />} title={name} subheader={displayDate} />
       <CardMedia component="img" height="400" image={image} alt="Paella dish" />
       <CardContent>
         <Typography variant="body2" color="secondary">
